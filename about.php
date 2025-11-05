924c08fe23a718caf80e7843ec06e0387ca6a887
unknown <vasudhas@gmail.com>
2025-11-05 12:21:01 +0530

    checking origin main

--- about.php
+++ about.php
@@ -55,7 +55,7 @@ body{
       <a class="navbar-brand" href="adminlogin.php">Admin</a>
     </div>
     <ul class="nav navbar-nav">
-      <li class="active"><a href="home.php">Home</a></li>
+      <li class="active"><a href="home.php">Homeeee</a></li>
       <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Online application<span class="caret"></span>
       </a>
         <ul class="dropdown-menu">
